e9bcc8847e7b0078f4eb099e3d2f8cf0c1acb620
Stephen Barr (Vertex Development) <stephen.barr@devfactory.com>
2022-07-29 22:26:34 +0000

    Updating to generate a highlight with a simple offset

--- dlib.py
+++ dlib.py
@@ -8,7 +8,7 @@ import os
 from pymediainfo import MediaInfo
 from collections import namedtuple
 import uuid
-
+import shotstack 
 
 VidData = namedtuple("VidData", ["remote_uri", "duration_s"])
 
@@ -37,11 +37,13 @@ def make_image_clip(image_uri):
          })
 
 ## ------------------------------------------------------------------
-def make_vid_clip(vid_uri, start, duration):
+def make_vid_clip(vid_uri, start, duration, trim = 0):
     return({
               "asset": {
                 "type": "video",
-                "src": vid_uri
+                "src": vid_uri,
+                "volume" : 1,
+                "trim" : trim
               },
               "start": start,
               "length": duration
@@ -62,7 +64,7 @@ def construct_highlight_video(thumbnail_uri, vid_data):
     start_time = image_time
     for v in vid_data:
         tracks.append({"clips" : [make_vid_clip(v.remote_uri, start_time, v.duration_s)]})
-        start_time = start_time = v.duration_s
+        start_time = start_time + v.duration_s
 
     req_data = { "timeline" : { "tracks" : tracks},
                  "output": { 
@@ -71,12 +73,40 @@ def construct_highlight_video(thumbnail_uri, vid_data):
                  }
                 }
     print(req_data)
+    return(shotstack.test_merge(req_data))
 
 
+def construct_highlight_video_short(thumbnail_uri, vid_data):
+    # Get vids locally
+    # Get their duration
+
+
+    image_time = 2
+
+    tracks = []
+    tracks.append({"clips": [make_image_clip(thumbnail_uri)]})
+
+    start_time = image_time
+    for v in vid_data:
+        eff_duration = min(v.duration_s, 30)
+        if v.duration_s > 30:
+            tracks.append({"clips" : [make_vid_clip(v.remote_uri, start_time, eff_duration, trim=30)]})            
+        else:
+            tracks.append({"clips" : [make_vid_clip(v.remote_uri, start_time, eff_duration)]})
+        start_time = start_time + eff_duration
+
+    req_data = { "timeline" : { "tracks" : tracks},
+                 "output": { 
+                     "format" : "mp4",
+                     "resolution" : "sd"
+                 }
+                }
+    print(req_data)
+    return(shotstack.test_merge(req_data))
 
 def test_construct():
     vid_1 = VidData(remote_uri="https://devgraph-aws-made-easy.s3.amazonaws.com/hl-auto/macie-recode.mp4", duration_s=481)
     vid_2 = VidData(remote_uri= "https://devgraph-aws-made-easy.s3.amazonaws.com/hl-auto/twitter-end.mp4", duration_s=5)
     thumbnail_uri = "https://devgraph-aws-made-easy.s3.amazonaws.com/hl-auto/macie-thumb.jpeg"
-    construct_highlight_video(thumbnail_uri, [vid_1, vid_2])
+    r = construct_highlight_video_short(thumbnail_uri, [vid_1, vid_2])
     return None
